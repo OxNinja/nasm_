1b92b67c495169e60b532b6dfdc97947f5800d48
OxNinja <57400445+OxNinja@users.noreply.github.com>
2022-03-17 10:59:20 +0100

    89 bytes now! Changed opcodes to free one byte

--- elf-golfing/golf.s
+++ elf-golfing/golf.s
@@ -3,7 +3,7 @@
 ; 32 bits
 bits 32
 ; vaddr mapping
-org 0x80000
+org 0x08000000
 
 _eident:
   ;; header hardcoding
@@ -12,12 +12,10 @@ _eident:
   db 0x01               ; eident_data (big/little endian)
   db 0x01               ; eident_version (:shrug: not used)
   db 0x00               ; eident_osabi (linux, netbsd, solaris...)
-
-  db 0x00               ; eident_abiversion (:shrug: not used)
-  ;times 7 db 0x00       ; eident_pad (padding)
   ;; declaring variables in the wild
   str: db "Hello!", 0xa
   strl: equ $-str
+  db 0x00               ; eident_pad (padding)
 
   dw 0x02               ; e_type
   dw 0x03               ; e_machine (x86, mips...)
@@ -25,21 +23,29 @@ _eident:
   dd _start             ; e_entry
   dd _pht-$$            ; e_phoff (program header table)
   
-  dd 0x00               ; e_shoff (header table)
-  dd 0x00               ; e_flags (:shrug: depends on arch)
+;; exit part, last instructions of the program
+_exit:
+  int 0x80
+
+  ;; exit 0
+  mov al, 0x01
+  xor bl, bl
+  int 0x80
+;; end exit
+
   dw eident_s           ; e_ehsize (usually 52 or 64 bytes)
   dw pht_s              ; e_phentsize (size program header table entry)
+;; overlapping PHT on ELF header
 _pht:
-  ;; program header table overlapping ELF header
-  dw 0x01               ; e_phnum (num of program header table entries)                         p_type2
-  dw 0x00               ; e_shentsize (size of section header table entry, :shrug: not used)    P_type1
-  dw 0x00               ; e_shnum (num of section header table entries)                         p_offset2
-  dw 0x00               ; e_shstrndx (index of section header table with section names)         p_offset1
+  dw 0x01               ; e_phnum (num of program header table entries)                         p_type1
+  dw 0x00               ; e_shentsize (size of section header table entry, :shrug: not used)    p_type2
+  dw 0x00               ; e_shnum (num of section header table entries)                         p_offset1
+  dw 0x00               ; e_shstrndx (index of section header table with section names)         p_offset2
   ;; end header
 
 eident_s equ $-_eident
 
-  ;; continue pgrogram header table
+  ;; continue program header table
   dd $$                 ; p_vaddr (addr of segment)
   dd $$                 ; p_paddr (physical addr of segment)
   
@@ -50,17 +56,14 @@ eident_s equ $-_eident
   ;; end program header table
 pht_s equ $-_pht
 
+
 _start:
   ;; write Hello! to stdout
   mov al, 0x04
-  mov bl, 0x01
+  inc bl
   mov ecx, str
   mov dl, strl
-  int 0x80
 
-  ;; exit 0
-  mov al, 0x01
-  dec ebx ; ebx = 1
-  int 0x80
+  jmp short _exit
   
 file_s equ $-$$
